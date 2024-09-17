a5444c596b270c63d32544dcb3177a2a700c287b
Felopatear Alaa Fathy <feloalaa50@gmail.com>
2024-09-17 19:59:21 +0300

    fix firebase bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:quicklab/signup/signup_page.dart';
+
+import 'login/login_page.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,7 +17,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/signup',
+      routes: {
+        '/login': (context) => LoginScreen(),
+        '/signup': (context) => SignUpScreen(),
+      },
     );
   }
 }

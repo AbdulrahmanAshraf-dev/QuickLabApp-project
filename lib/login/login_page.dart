5927da0b75ce7359a691649225b6f1b667393dab
Felopatear Alaa Fathy <feloalaa50@gmail.com>
2024-09-17 19:39:16 +0300

    firebase init

---
+++ lib/login/login_page.dart
@@ -0,0 +1,224 @@
+
+import 'package:flutter/material.dart';
+
+
+
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _buildTopIcon(),
+              const SizedBox(height: 50),
+              _buildLoginTitle(),
+              const SizedBox(height: 30),
+              _buildEmailTextField(),
+              const SizedBox(height: 20),
+              _buildPasswordTextField(),
+              const SizedBox(height: 10),
+              _buildForgotPasswordButton(),
+              const SizedBox(height: 20),
+              _buildLoginButton(),
+              const SizedBox(height: 30),
+              _buildOrLoginWith(),
+              const SizedBox(height: 40),
+              _buildSocialLoginButtons(),
+              const SizedBox(height: 100),
+              _buildSignUpText(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTopIcon() {
+    return Container(
+      alignment: Alignment.topCenter,
+      child: Column(
+        children: [
+          Image.asset(
+            'assets/images/lap.png',
+            height: 100,
+          ),
+          const Text(
+            'QuickLap',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLoginTitle() {
+    return const Column(
+      children: [
+        Text(
+          'Log In',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        SizedBox(height: 5),
+        Text(
+          'Use your credentials and login to your account',
+          style: TextStyle(
+            fontSize: 14,
+            color: Colors.grey,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildEmailTextField() {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: 'Enter your email',
+        filled: true,
+        fillColor: const Color(0xFFF3F5F7),
+        prefixIcon: const Icon(Icons.email, color: Colors.grey),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: BorderSide.none,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPasswordTextField() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Enter your password',
+        filled: true,
+        fillColor: const Color(0xFFF3F5F7),
+        prefixIcon: const Icon(Icons.lock, color: Colors.grey),
+        suffixIcon: const Icon(Icons.visibility, color: Colors.grey),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: BorderSide.none,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildForgotPasswordButton() {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: TextButton(
+        onPressed: () {},
+        child: const Text(
+          'Forgot Password?',
+          style: TextStyle(
+            color: Colors.purple,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoginButton() {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color(0xFF6C5DD3),
+        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 120),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+      child: const Text(
+        'Log In',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOrLoginWith() {
+    return const Text(
+      'Or Log in with',
+      style: TextStyle(
+        color: Colors.grey,
+        fontSize: 14,
+      ),
+    );
+  }
+
+  Widget _buildSocialLoginButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _buildSocialBtn('assets/images/apple.png'), // Phone Icon
+        _buildSocialBtn('assets/images/facebook.png'), // Facebook Icon
+        _buildSocialBtn('assets/images/google.png'), // Google Icon
+      ],
+    );
+  }
+
+  Widget _buildSignUpText(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('if you are new', style: TextStyle(fontSize: 14)),
+        TextButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/signup'); // Navigate to Sign Up Page
+          },
+          child: const Text('Create New Account', style: TextStyle(fontSize: 14, color: Color(0xFF9B51E0))),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSocialBtn(String imagePath) {
+    return GestureDetector(
+      onTap: () {
+        print('Social button pressed');
+      },
+      child: Container(
+        height: 60,
+        width: 60,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              spreadRadius: 2,
+              blurRadius: 6,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Center(
+          child: Image.asset(
+            imagePath,
+            height: 30,
+            width: 30,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/signup/signup_page.dart
@@ -0,0 +1,194 @@
+import 'package:flutter/material.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import '../login/login_page.dart';
+
+class SignUpScreen extends StatefulWidget {
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  bool _isChecked = false;
+  PhoneNumber _phoneNumber = PhoneNumber(isoCode: 'US');
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _buildLogo(),
+              SizedBox(height: 50),
+              _buildTitle(),
+              SizedBox(height: 5),
+              _buildSubtitle(),
+              SizedBox(height: 30),
+              _buildTextField('Enter full name', false),
+              SizedBox(height: 20),
+              _buildTextField('Enter your email', false),
+              SizedBox(height: 20),
+              _buildPhoneNumberField(),
+              SizedBox(height: 20),
+              _buildTextField('Enter your password', true),
+              SizedBox(height: 12),
+              _buildTermsCheckbox(),
+              SizedBox(height: 12),
+              _buildSignUpButton(),
+              SizedBox(height: 12),
+              _buildLoginText(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLogo() {
+    return Column(
+      children: [
+        Image.asset(
+          'assets/images/lap.png', // Ensure this path is correct
+          height: 100,
+        ),
+        Text(
+          'QuickLap',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTitle() {
+    return Text(
+      'Sign Up',
+      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    );
+  }
+
+  Widget _buildSubtitle() {
+    return Text(
+      'Use your details to create a new account!',
+      style: TextStyle(color: Colors.grey[600], fontSize: 14),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Widget _buildTextField(String hintText, bool isPassword) {
+    return TextField(
+      obscureText: isPassword,
+      decoration: InputDecoration(
+        hintText: hintText,
+        filled: true,
+        fillColor: Colors.grey[200],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+        contentPadding: EdgeInsets.symmetric(vertical: 20, horizontal: 12),
+        suffixIcon: isPassword ? Icon(Icons.visibility_off) : null,
+      ),
+    );
+  }
+
+  Widget _buildPhoneNumberField() {
+    return InternationalPhoneNumberInput(
+      onInputChanged: (PhoneNumber number) {
+        setState(() {
+          _phoneNumber = number;
+        });
+      },
+      onInputValidated: (bool value) {
+        // Handle validation result
+      },
+      selectorConfig: SelectorConfig(
+        selectorType: PhoneInputSelectorType.DROPDOWN,
+        showFlags: true,
+      ),
+      ignoreBlank: false,
+      autoValidateMode: AutovalidateMode.onUserInteraction,
+      formatInput: false,
+      cursorColor: Colors.black,
+      keyboardType: TextInputType.numberWithOptions(signed: true, decimal: true),
+      inputDecoration: InputDecoration(
+        hintText: 'Enter phone number',
+        filled: true,
+        fillColor: Colors.grey[200],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+        contentPadding: EdgeInsets.symmetric(vertical: 20, horizontal: 12),
+      ),
+    );
+  }
+
+  Widget _buildTermsCheckbox() {
+    return Row(
+      children: [
+        Checkbox(
+          value: _isChecked,
+          onChanged: (bool? value) {
+            setState(() {
+              _isChecked = value ?? false;
+            });
+          },
+          checkColor: Colors.white,
+          activeColor: Colors.green, // Change the color when checked
+        ),
+        Expanded(
+          child: Text(
+            'I agree with Terms & Privacy',
+            style: TextStyle(fontSize: 14),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSignUpButton() {
+    return ElevatedButton(
+      onPressed: _isChecked ? () {} : null, // Disable if checkbox is not checked
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Color(0xFF6C5DD3),
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 120),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+      child: Text(
+        'Sign Up',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoginText(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('Already have an account?', style: TextStyle(fontSize: 14)),
+        TextButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/login'); // Navigate to Login Page
+          },
+          child: Text('Login', style: TextStyle(fontSize: 14, color: Color(0xFF9B51E0))),
+        ),
+      ],
+    );
+  }
+}
